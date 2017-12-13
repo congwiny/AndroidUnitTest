1136ed225872f4a53fa10c87d0a179d19efd3a22
Niuchong <niuchong@360.cn>
2017-12-13 19:23:06 +0800

    Add:Mockito Argument Matcher

---
+++ app/src/test/java/com/congwiny/androidunittest/mockito/Mockito_7_MatchersTest.java
@@ -0,0 +1,61 @@
+package com.congwiny.androidunittest.mockito;
+
+import com.congwiny.androidunittest.bean.Person;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
+
+public class Mockito_7_MatchersTest {
+
+    @Mock
+    Person mPerson;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Test
+    public void testPersonAny(){
+        when(mPerson.eat(any(String.class))).thenReturn("米饭");
+        //或
+        //when(mPerson.eat(anyString())).thenReturn("米饭");
+
+        //输出米饭
+        System.out.println(mPerson.eat("面条"));
+
+    }
+
+
+    @Test
+    public void testPersonContains(){
+
+        when(mPerson.eat(contains("面"))).thenReturn("面条");
+        //输出面条
+        System.out.println(mPerson.eat("面"));
+
+    }
+
+    @Test
+    public void testPersonArgThat(){
+
+        //自定义输入字符长度为偶数时，输出面条。
+        when(mPerson.eat(argThat(new ArgumentMatcher<String>() {
+            @Override
+            public boolean matches(String argument) {
+                return argument.length() % 2 == 0;
+            }
+        }))).thenReturn("面条");
+        //输出面条
+        System.out.println(mPerson.eat("1234"));
+
+    }
+
+}
