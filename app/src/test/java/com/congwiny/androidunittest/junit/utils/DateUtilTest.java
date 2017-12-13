425acc10b88218588bb4689b7fe4d63d6c54619f
Niuchong <niuchong@360.cn>
2017-12-13 16:05:43 +0800

    Add:junit hello world

---
+++ app/src/test/java/com/congwiny/androidunittest/junit/utils/DateUtilTest.java
@@ -0,0 +1,43 @@
+package com.congwiny.androidunittest.junit.utils;
+
+import com.congwiny.androidunittest.utils.DateUtil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class DateUtilTest {
+
+    private String time = "2017-10-15 16:00:02";
+
+    private long timeStamp = 1508054402000L;
+    private Date mDate;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("测试开始！");
+        mDate = new Date();
+        mDate.setTime(timeStamp);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("测试结束！");
+    }
+
+    @Test(expected = ParseException.class)
+    public void dateToStamp() throws Exception {
+        //assertEquals(time, DateUtil.stampToDate(timeStamp));
+        DateUtil.dateToStamp("2017-10-15");
+    }
+
+    @Test
+    public void stampToDate() throws Exception {
+        assertNotEquals(4, DateUtil.dateToStamp(time));
+    }
+}
